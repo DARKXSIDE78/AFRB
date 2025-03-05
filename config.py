3297dde4ddde18e7b5d82f226383c4ee0ba8adb4
Fraxx78 <fraxxteam@outlook.com>
2025-03-05 04:51:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -21,6 +21,7 @@ class Config(object):
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', '@Bots_Nation').split(',')
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002385922224")
     DUMP_CHANNEL = os.environ.get("DUMP_CHANNEL", "-1002264269433")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "AutoRenamerXBOt")
     #TOKEN_API = "https://instantearn.in/api?api=fa0dc64a5224ed38ec7b25c70f40922a1f8aeb15&format=text"
     TOKEN_API = "533ce3f92f47b35345541ac59f7010de31fbeba7"
     SHORTENER_URL = "https://droplink.co/api"
