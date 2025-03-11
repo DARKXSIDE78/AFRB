c6d6ab32f4df873426b889209b4e360dad868dce
DARKXSIDE78 <nk1010057@gmail.com>
2025-03-11 11:58:37 +0930

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from config import Config
 from aiohttp import web
 from route import web_server
 import pyrogram.utils
-import pyromod
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
 import time
@@ -37,7 +36,7 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
+        self.uptime = Config.BOT_UPTIME  
         if Config.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()       
